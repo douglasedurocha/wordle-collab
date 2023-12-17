ad0bb8fda407a35f01dfb001b089164dbb033a53
Douglas Eduardo Rocha <douglasedurocha@gmail.com>
2023-12-16 22:45:17 -0300

    update games endpoint

--- wordle-collab/core/api/urls.py
+++ wordle-collab/core/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path("", include("game.api.urls")),
+    path("games/", include("game.api.urls")),
     path("accounts/", include("accounts.api.urls")),
 ]
