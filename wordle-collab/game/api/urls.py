75b19cc74ff1722c50fb7892e19712d06bc68a19
Douglas Eduardo Rocha <douglasedurocha@gmail.com>
2023-12-16 22:40:55 -0300

    add open game list API view

--- wordle-collab/game/api/urls.py
+++ wordle-collab/game/api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("open", views.OpenGameListView.as_view(), name="open-games"),
     path("create", views.GameCreateView.as_view(), name="create-game"),
 ]
--- wordle-collab/game/api/views.py
+++ wordle-collab/game/api/views.py
@@ -1,4 +1,4 @@
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import GameSerializer
@@ -14,3 +14,13 @@ class GameCreateView(APIView):
         game = Game.objects.create(word="teste")
         game.add_player(request.user)
         return Response(GameSerializer(game).data, status=201)
+
+
+class OpenGameListView(APIView):
+    permission_classes = [
+        AllowAny,
+    ]
+
+    def get(self, request):
+        games = Game.objects.filter(status="W")
+        return Response(GameSerializer(games, many=True).data, status=200)
