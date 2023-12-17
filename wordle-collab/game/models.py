44437a5d61732bd262407642ebbf2784d0e067e4
Douglas Eduardo Rocha <douglasedurocha@gmail.com>
2023-12-16 21:00:23 -0300

    add game model and update attempt model

--- wordle-collab/game/models.py
+++ wordle-collab/game/models.py
@@ -1,9 +1,28 @@
-from django.db import models  # F401
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class Game(models.Model):
+    word = models.CharField(max_length=5)
+    players = models.ManyToManyField(User)
+    max_players = models.IntegerField(default=2)
+    max_atempts = models.IntegerField(default=5)
+
+    def add_player(self, player):
+        if self.players.count() >= self.max_players:
+            return
+        self.players.add(player)
+        self.save()
+        return
 
 
 # Create your models here.
 class Attempt(models.Model):
+    player = models.ForeignKey(User, on_delete=models.CASCADE)
     word = models.CharField(max_length=5)
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
 
     def __str__(self) -> str:
         return self.word
